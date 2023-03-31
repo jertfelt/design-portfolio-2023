c1597bc0ab57cb94c843f55a4454576a1151b4d8
Tova Jertfelt <tovizitan@gmail.com>
2023-03-31 19:48:48 +0200

    testar att ändra i next-i18

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,10 +1,26 @@
 const path = require("path");
+const languages = require("./i18n.languages.js");
 
-module.exports = {
-  i18n: {
-    locales: ["sv", "en"],
-    defaultLocale: "sv",
-    localeDetection: false,
-    localePath: path.resolve("./public/locales"),
-  },
-};
+const i18n = {
+  defaultLocale: "sv",
+  locales: languages,
+  localeDetection: false
+}
+
+/** @type import("next-i18next").UserConfig */
+const next18nextConfig={
+  i18n,
+  fallbackLng: "sv",
+  localePath: path.resolve("./public/locales"),
+}
+
+module.exports = next18nextConfig;
+
+// module.exports = {
+//   i18n: {
+//     locales: ["sv", "en"],
+//     defaultLocale: "sv",
+//     localeDetection: false,
+//     localePath: path.resolve("./public/locales"),
+//   },
+// };
--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
-/** @type {import('next').NextConfig} */
+
 const { i18n } = require("./next-i18next.config");
 
-const nextConfig = {
+/** @type {import('next').NextConfig} */
+
+const config = {
   reactStrictMode: true,
   swcMinify: true,
   i18n,
 }
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   i18n,
+// }
 
-module.exports = nextConfig
+module.exports = config;
