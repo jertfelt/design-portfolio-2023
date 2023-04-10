f84d8df5ecc9869bb7d602d8ec2bb98c7832de44
Tova Jertfelt <tovizitan@gmail.com>
2023-04-10 19:49:43 +0200

    need to look at width properties in imagewrapper

--- src/components/Art/ViKanBattre.jsx
+++ src/components/Art/ViKanBattre.jsx
@@ -1,5 +1,14 @@
 import styled from "styled-components";
 import Image from "next/image";
+import vikan1 from "../../../public/art/vi-kan-battre-tova-jertfelt-kandidatexamen-2016-konstfack--2.png"
+import vikan2 from "../../../public/art/vi-kan-battre-tova-jertfelt-kandidatexamen-2016-konstfack--3.png"
+import vikan3 from "../../../public/art/vi-kan-battre-tova-jertfelt-kandidatexamen-2016-konstfack--4.png"
+import vikan4 from "../../../public/art/vi-kan-battre-tova-jertfelt-kandidatexamen-2016-konstfack--5.png"
+import vikan5 from "../../../public/art/vi-kan-battre-tova-jertfelt-kandidatexamen-2016-konstfack--print.png"
+import vikan6 from "../../../public/art/vi-kan-battre-tova-jertfelt-kandidatexamen-2016-konstfack--print2.png"
+import vikan7 from "../../../public/art/vi-kan-battre-tova-jertfelt-kandidatexamen-2016-konstfack.png"
+import Zoom from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
 import { useTranslation } from "next-i18next";
 import Link from "next/link";
 import ReactPlayer from 'react-player/lazy'
@@ -9,8 +18,8 @@ min-height:50vh;
 background: ${({theme}) => theme.artcolors.primaryLighter};
 padding:2rem;
 color:${({theme}) => theme.textSecondary};
-padding-bottom:0;
-margin-bottom:-8px;
+
+
 h2{
   color:${({theme}) => theme.textPrimary};
   font-size:10vh;
@@ -34,6 +43,19 @@ div{
   max-width:100vw;
 }
 `
+const ImageWrapper = styled.div`
+margin:0;
+padding:0;
+margin-left:-2rem;
+width:100%;
+max-width:90vw;
+position:relative;
+img{
+  width:100vw;
+  height: 70vh;
+}
+
+`
 
 const ViKanBattre = () => {
   const {t} = useTranslation()
@@ -45,12 +67,38 @@ const ViKanBattre = () => {
       <p>{t("artistpages.vikanbattre.smalldescription")}
       </p>
     <Link href="http://www.diva-portal.org/smash/record.jsf?pid=diva2%3A936493&dswid=9026" target="_blank">{t("artistpages.vikanbattre.readLink")}</Link>
+    <ImageWrapper>
+<Zoom>
+<Image src={vikan1}
+className="customzoom"
+alt="Part One, showing some of the paintings"
+></Image>
+</Zoom>
+<Zoom>
+<Image src={vikan2}
+className="customzoom"
+alt="Part One, showing some of the paintings"
+></Image>
+</Zoom>
+<Zoom>
+<Image src={vikan3}
+className="customzoom"
+alt="Part One, showing some of the paintings"
+></Image>
+</Zoom>
+</ImageWrapper>
     <VideoWrapper>
     <ReactPlayer url='https://youtu.be/IfffzYAGps4' 
     />
     </VideoWrapper>
     <p>{t("artistpages.vikanbattre.material")}</p>
     <p>{t("artistpages.vikanbattre.description1")}</p>
+    <Zoom>
+<Image src={vikan5}
+className="customzoom"
+alt="Part One, showing some of the paintings"
+></Image>
+</Zoom>
     </Section>
     );
 }
