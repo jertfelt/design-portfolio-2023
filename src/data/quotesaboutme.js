7b128eff73bfbbfa957f332637f87e37d32757ec
Tova Jertfelt <tovizitan@gmail.com>
2023-11-28 10:16:24 +0100

    fixat till ordning

--- src/data/quotesaboutme.js
+++ src/data/quotesaboutme.js
@@ -1,25 +1,32 @@
 export const quotesAboutMe = [
+  {id:0,
+    headline: "Ett starkt tillskott.",
+    quote:"Tova är en otroligt snabblärd och engagerad frontend utvecklare med ett stort sinne för UX. Tova klarar av att ha koll på många saker samtidigt och utför sitt arbete noggrant. Tova smälter lätt in i gruppen och ser till att den växer, detta genom sitt individuella ansvar i de vardagliga arbetsuppgifter hon har, hennes förståelse kring teamets arbetssätt och att se vilka möjligheter som finns för förbättring. Tova delar gärna med sig av de kompetenser hon besitter till övriga, och ställer professionellt frågor som leder till att mål uppnås. Jag skickar mina varmaste rekommendationer till de som vill ha en effektiv, lösningsorienterad, humoristisk, samarbetsvillig och medmänsklig kollega.",
+    name:"– Basil Madani, Frontend-utvecklare Arbetsförmedlingen"
+  },
+  {id:0,
+    headline: "Iniativrik och investerad",
+    quote:"Hade ynnesten att få jobba med Tova för ett projekt för Greenpeace.  Tova var väldigt lyhörd för förändringar och ideer till upplägget.Hon tog även mycket egna initiativ och fyllde i de designhål i upplevelsen som jag inte hunnit till. Det var tydligt att Tova var lika investerad i slutupplevelsen som resten av teamet. Då Tova har en bakgrund i visuell kommunikation gör det henne till en utvecklare som en designer verkligen uppskattar att samarbeta med.",
+    name:"– Per Lundgren, Senior UX Designer, Vinnovera"
+  },
+
 
   {id:2,
     headline: "Kompetent",
     quote:"Tova har mycket goda kunskaper i hur och varför man arbetar med JavaScript inom frontendutveckling och kan med säkerhet välja rätt metoder.",
-    name:"– Harald Mellbom, Vinnovera"
+    name:"– Harald Mellbom, Scrummaster, Vinnovera"
   },
-  {id:0,
-    headline: "Ett starkt tillskott.",
-    quote:"Tova är en otroligt snabblärd och engagerad frontend utvecklare med ett stort sinne för UX. Tova klarar av att ha koll på många saker samtidigt och utför sitt arbete noggrant. Tova smälter lätt in i gruppen och ser till att den växer, detta genom sitt individuella ansvar i de vardagliga arbetsuppgifter hon har, hennes förståelse kring teamets arbetssätt och att se vilka möjligheter som finns för förbättring. Tova delar gärna med sig av de kompetenser hon besitter till övriga, och ställer professionellt frågor som leder till att mål uppnås. Jag skickar mina varmaste rekommendationer till de som vill ha en effektiv, lösningsorienterad, humoristisk, samarbetsvillig och medmänsklig kollega.",
-    name:"– Basil Madani, frontend-utvecklare"
-  },
+ 
   {id:3,
     headline: "Omtänksam & Driven",
     quote:"Tova är väldigt driven och engagerad med ett bra öga för UX och en väldigt omtänksam medmänniska och arbetskamrat som tänker på teamets bästa i alla lägen..",
-    name:"– Christian Fujimaki Ahlsén, frontend-utvecklare"
+    name:"– Christian Fujimaki Ahlsén, frontend-utvecklare, Arbetsförmedlingen"
   },
-  {
-    id:8,
-    headline:"Driven",
-    quote:"Tova är mycket kompetent och driven.",
-    name:"– Fredrika, klasskamrat Nackademin"
+ 
+  {id:7,
+    headline: "Nyfiken & engagerad",
+    quote:"Tova lär sig snabbt och gärna, har en hög arbetsmoral och är väldigt lätt att arbeta tillsammans med. Utöver att vara en duktig programmerare så har hon, tack vare sin konstnärliga bakgrund även en unik förståelse för design/UX/UI. Jag rekommenderar henne starkt!",
+    name:"– Vidar Rehn, frontend-utvecklare"
   },
   {
     id:1,
@@ -31,22 +38,19 @@ export const quotesAboutMe = [
     headline:"Generös",
     quote:"Tova är hjälpsam, kunnig och lyhörd inför önskemål.",
     name:"– Globalportalen"
-  }, {id:7,
-    headline: "Nyfiken & engagerad",
-    quote:"Tova lär sig snabbt och gärna, har en hög arbetsmoral och är väldigt lätt att arbeta tillsammans med. Utöver att vara en duktig programmerare så har hon, tack vare sin konstnärliga bakgrund även en unik förståelse för design/UX/UI. Jag rekommenderar henne starkt!",
-    name:"– Vidar Rehn, frontend-utvecklare"
   },
-
+  {
+    id:8,
+    headline:"Driven",
+    quote:"Tova är mycket kompetent och driven.",
+    name:"– Fredrika, klasskamrat Nackademin"
+  },
   
 ]
 
 export const quotesAboutMe_EN = [
 
-  {id:1,
-    headline: "Knowledgeable",
-    quote:"Tova has good knowledge in how, and why, you work with JavaScript within frontend development, and can with certainty choose right methods",
-    name:"– Harald Mellbom, Vinnovera"
-  },
+
   {id:6,
   headline:"Invested & Responsive ",
 quote:"I had the privilege of working with Tova on a project for Greenpeace.  Tova was very responsive to changes and ideas for the layout. She also took a lot of personal initiatives and filled in the design gaps in the experience that I hadn’t had time to address. It was clear that Tova was equally invested in the final experience as the rest of the team. With Tova’s background in visual communication, she becomes a developer that a designer truly appreciates collaborating with.",
@@ -55,6 +59,11 @@ name:"– Per Lundgren, Vinnovera"},
   headline: "A strong addition",
   quote:"Tova is an incredibly quick and dedicated frontend developer with a keen sense of UX. Tova can handle many things at once and performs her work meticulously. She easily integrates into the team and contributes to its growth through her individual responsibility in daily tasks, understanding of the team’s workflow, and identifying opportunities for improvement. Tova willingly shares her skills with others and poses professionally formulated questions that lead to achieving goals. I send my warmest recommendations to those who seek an efficient, solution-oriented, humorous, collaborative, and compassionate colleague.",
   name:"– Basil Madani, Arbetsförmedlingen"
+},
+{id:1,
+  headline: "Knowledgeable",
+  quote:"Tova has good knowledge in how, and why, you work with JavaScript within frontend development, and can with certainty choose right methods",
+  name:"– Harald Mellbom, Vinnovera"
 },
 {
   id:2,
