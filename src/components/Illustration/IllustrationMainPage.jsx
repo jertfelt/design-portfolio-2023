ed4799cdab5c1b91940c2a77fe5f05a7f06d9860
Tova Jertfelt <tovajertfelt@gmail.com>
2024-02-07 13:19:11 +0100

    error vid illustration, fixat

--- src/components/Illustration/IllustrationMainPage.jsx
+++ src/components/Illustration/IllustrationMainPage.jsx
@@ -1,21 +1,18 @@
 import styled from "styled-components";
 import WorkInProgress from "../WorkInProgress";
 
-
 const Section = styled.div`
-min-height:120vh;
-padding:0;
-margin:0;
-color: ${({theme}) => theme.primaryText};
-background: ${({theme}) => theme.designcolors.primary};
-
-`
-
+  min-height: 120vh;
+  padding: 0;
+  margin: 0;
+`;
 
 const IlluMainPage = () => {
-  return (<Section>
-    <WorkInProgress></WorkInProgress>
-  </Section>  );
-}
- 
+  return (
+    <Section>
+      <WorkInProgress></WorkInProgress>
+    </Section>
+  );
+};
+
 export default IlluMainPage;
