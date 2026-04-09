ca717f7e90fc05873d2f0b287c12a24107ed1e9d
Tova Jertfelt <tovajertfelt@gmail.com>
2026-04-09 10:13:41 +0200

    testar att importera getstatic på nytt sätt

--- src/components/utils/getStaticProps.js
+++ src/components/utils/getStaticProps.js
@@ -1,21 +1,21 @@
-import {serverSideTranslations} from "next-i18next/serverSideTranslations"
+export async function getStaticProps({ locale }) {
+  const { serverSideTranslations } =
+    await import("next-i18next/serverSideTranslations");
 
-
-export async function getStaticProps({locale}){
-  return{
-    props:{
-      ...(await serverSideTranslations(locale, ["common"]))
-    }
-  }
+  return {
+    props: {
+      ...(await serverSideTranslations(locale ?? "en", ["common"])),
+    },
+  };
 }
 
 const withTranslation = (Component) => {
-  const wrappedComponent = props => (
+  const wrappedComponent = (props) => (
     <>
-    <Component {...props}/>
+      <Component {...props} />
     </>
-  )
+  );
   return wrappedComponent;
-}
+};
 
 export default withTranslation;
